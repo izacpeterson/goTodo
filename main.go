f905edf640e1f65cdb91f7580b6262b99b79da33
izacpeterson <izacpeterson@gmail.com>
2024-06-04 18:55:13 -0600

    add item

--- main.go
+++ main.go
@@ -1,18 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Welcome to your Todo List")
-	fmt.Printf("Commands\nX + <line number>: delete\nI + <todo item>: insert\nQ: exit\n")
+	fmt.Printf("Commands: X + <line number>: delete, I + <todo item>: insert, Q: exit\n")
 	var input string
 
-	for input != "q" {
+	todoList := loadList()
+
+	printList(todoList)
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+
 		fmt.Print("Action: ")
-		fmt.Scan(&input)
-		fmt.Println("You entered: ", input)
+		scanner.Scan()
+		input = scanner.Text()
+
+		if strings.ToLower(input) == "q" {
+			break
+		}
+
+		action := input[0]
+		content := strings.TrimSpace(input[1:])
+
+		switch action {
+		case 'i', 'I':
+			todoList = append(todoList, content)
+			printList(todoList)
+		}
 	}
 
 	fmt.Println("Goodbye")
 
+}
+
+func loadList() []string {
+	file, err := os.Open("tasks.tsk")
+	if err != nil {
+		fmt.Println("ERROR Opening File: ", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	var todoList []string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		todoList = append(todoList, scanner.Text())
+	}
+
+	return todoList
+}
+
+func printList(todoList []string) {
+	println()
+	for i := 0; i < len(todoList); i++ {
+		fmt.Printf("%v: %v\n", i, todoList[i])
+	}
+	println()
 }
